856415c9749e22a5b0d3049f67dc7a021c5b65a8
divyanshBuyhatke <divyansh@buyhatke.com>
2021-11-08 12:38:37 +0530

    Improved Recommendation Engine

--- app.py
+++ app.py
@@ -15,37 +15,59 @@ import math
 app = Flask(__name__)
 app.config.from_object('config')
 
-df=0
-vectorizer=0
-model=0
+df = 0
+vectorizer = 0
+model = 0
+dfBigCluster = 0
+vectorizerCluster = 0
+modelCluster = 0
+largestClusterId = 0
+
+
 def preprocess():
     global df
-    df=pd.read_csv('assets/finalData.csv')
+    global model
     global vectorizer
-    vectorizer=TfidfVectorizer(stop_words='english')
-    inp=vectorizer.fit_transform(df['title'].values.astype('U'))
-    fp= open('assets/recommender_model_kmeans.pkl', 'rb')
-    global model 
-    model= pickle.load(fp)
+    global dfBigCluster
+    global modelCluster
+    global vectorizerCluster
+    global largestClusterId
+    df = pd.read_csv('assets/finalData.csv')
+    vectorizer = TfidfVectorizer(stop_words='english')
+    vectorizer.fit_transform(df['title'].values.astype('U'))
+    fp = open('assets/recommender_model_kmeans.pkl', 'rb')
+    model = pickle.load(fp)
+    k = 150
+    for i in range(k):
+        if(len(df[model.labels_ == i]) > len(df[model.labels_ == largestClusterId])):
+            largestClusterId = i
+
+    dfBigCluster = df[model.labels_ == largestClusterId]
+    vectorizerCluster = TfidfVectorizer(stop_words='english')
+    vectorizerCluster.fit_transform(dfBigCluster['title'].values.astype('U'))
+    fp2 = open('assets/resolveBigCluster.pkl', 'rb')
+    modelCluster = pickle.load(fp2)
     return
+
+
 def parseLink(imageList):
-    imageList=str(imageList)
-    first=-1
-    sec =-1
-    cur=""
+    imageList = str(imageList)
+    first = -1
+    sec = -1
+    cur = ""
     for i in range(len(imageList)):
-        if(imageList[i]=="'"):
-            if(first==-1):
-                first=i
+        if(imageList[i] == "'"):
+            if(first == -1):
+                first = i
             else:
-                sec=i
-                if(sec-first-1<=1):
-                    first=-1
-                    sec=-1
-                else :
+                sec = i
+                if(sec-first-1 <= 1):
+                    first = -1
+                    sec = -1
+                else:
                     return imageList[first+1:sec]
     return cur
-            
+
 
 #----------------------------------------------------------------------------#
 # Controllers.
@@ -54,67 +76,52 @@ def parseLink(imageList):
 
 @app.route('/')
 def home():
-    msg="This is a flask app"
+    msg = "This is a flask app"
     return msg
 
 
-@app.route('/getrecommendation',methods=['GET'])
+@app.route('/getrecommendation', methods=['GET'])
 def getrecommendationt():
-    title = request.args.get('title', default = '*', type = str)
+    title = request.args.get('title', default='*', type=str)
     print(title)
-    y=vectorizer.transform([title])
-    clusterId=model.predict(y)
-    df_cluster=df[model.labels_ == clusterId]
+    y = vectorizer.transform([title])
+    clusterId = model.predict(y)
+    df_cluster = df
     print("Cluster id:: ", clusterId)
-    result=[]
-    # sample=df_cluster.head(10)
-    # for i in range(len(sample)):
-    #     item=sample.iloc[i]
-    #     # print(item,type(item))
-    #     if(item['notThrift']==0):
-    #         continue
-    #     if(len(result)>=5):
-    #         break
-    #     obj={
-    #         'title':str(item['title']),
-    #         'brand':str(item['brand']),
-    #         'productId':int(item['productId']),
-    #     }
-    #     if(type(item['price'])==str or math.isnan(float(item['price']))==False):
-    #         obj['price']=str(item['price'])
-    #     # print(type(item['imageURLHighRes']))
-    #     if(type(item['imageURLHighRes'])==str):
-    #         # print(item['imageURLHighRes'])
-    #         # link=parseLink(item['imageURLHighRes'])
-    #         link=item['imageURLHighRes']
-    #         obj['imageURL']=link
-    #     result.append(obj)
-    sample=df_cluster.sample(25)
+    if(clusterId == largestClusterId):
+        y = vectorizerCluster.transform([title])
+        clusterId = modelCluster.predict(y)
+        df_cluster = dfBigCluster[modelCluster.labels_ == clusterId]
+        print("Small Cluster Id: ", clusterId)
+    else:
+        df_cluster = df[model.labels_ == clusterId]
+    result = []
+    sample = df_cluster.sample(25)
     for i in range(len(sample)):
-        item=sample.iloc[i]
+        item = sample.iloc[i]
         # print(item,type(item))
-        if(item['notThrift']==0):
+        if(item['notThrift'] == 0):
             continue
-        if(len(result)>=15):
+        if(len(result) >= 15):
             break
-        obj={
-            'title':str(item['title']),
-            'brand':str(item['brand']),
-            'productId':int(item['productId']),
+        obj = {
+            'title': str(item['title']),
+            'brand': str(item['brand']),
+            'productId': int(item['productId']),
         }
-        if(type(item['price'])==str or math.isnan(float(item['price']))==False):
-            obj['price']=str(item['price'])
+        if(type(item['price']) == str or math.isnan(float(item['price'])) == False):
+            obj['price'] = str(item['price'])
         # print(type(item['imageURLHighRes']))
-        if(type(item['imageURLHighRes'])==str):
+        if(type(item['imageURLHighRes']) == str):
             # print(item['imageURLHighRes'])
             # link=parseLink(item['imageURLHighRes'])
-            link=item['imageURLHighRes']
-            obj['imageURL']=link
+            link = item['imageURLHighRes']
+            obj['imageURL'] = link
         result.append(obj)
     # print(result)
-        
-    result={
-        'data':result
+
+    result = {
+        'data': result
     }
     return result
 
--- train.py
+++ train.py
@@ -2,30 +2,84 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 import pickle
+import os
 
 print("Importing Data...")
-df=pd.read_csv('assets/finalData.csv')
+df = pd.read_csv('assets/finalData.csv')
 print("Vectorize...")
-vectorizer=TfidfVectorizer(stop_words='english')
-inp=vectorizer.fit_transform(df['title'].values.astype(str))
+vectorizer = TfidfVectorizer(stop_words='english')
+inp = vectorizer.fit_transform(df['title'].values.astype(str))
 
-k=150
+k = 150
 print("Training...")
-model=KMeans(n_clusters=k,init='k-means++',n_init=10)
-model.fit(inp)
+model = 0
+if(os.path.exists("assets/recommender_model_kmeans.pkl")):
+    fp = open('assets/recommender_model_kmeans.pkl', 'rb')
+    model = pickle.load(fp)
+    print("Model path exists...")
+else:
+    model = KMeans(n_clusters=k, init='k-means++', n_init=10)
+    model.fit(inp)
 
 print("Top terms per cluster:")
 order_centroids = model.cluster_centers_.argsort()[:, ::-1]
 terms = vectorizer.get_feature_names()
+largestClusterId = 0
 for i in range(k):
     print("Cluster %d:" % i)
-    print("Size of cluster :: ",len(df[model.labels_==i]))
+    print("Size of cluster :: ", len(df[model.labels_ == i]))
+    if(len(df[model.labels_ == i]) > len(df[model.labels_ == largestClusterId])):
+        largestClusterId = i
     for ind in order_centroids[i, :5]:
         print(' %s' % terms[ind])
     print("*"*100)
 
-y=vectorizer.transform(['Sexy'])
-pred=model.predict(y)
+df2 = df[model.labels_ == largestClusterId]
+print("Vectorize 2 ...")
+vectorizerCluster = TfidfVectorizer(stop_words='english')
+inp = vectorizerCluster.fit_transform(df2['title'].values.astype(str))
+
+k2 = 15
+print("Training 2 ...")
+model2 = KMeans(n_clusters=k2, init='k-means++', n_init=10)
+model2.fit(inp)
+
+print("Top terms per cluster2 :")
+order_centroids = model2.cluster_centers_.argsort()[:, ::-1]
+terms = vectorizerCluster.get_feature_names()
+for i in range(k2):
+    print("Cluster %d:" % i)
+    print("Size of cluster2 :: ", len(df2[model2.labels_ == i]))
+    for ind in order_centroids[i, :5]:
+        print(' %s' % terms[ind])
+    print("*"*100)
+
+
+title = "Men's Trouser"
+y = vectorizer.transform([title])
+pred = model.predict(y)
+if(pred == largestClusterId):
+    y = vectorizerCluster.transform([title])
+    pred = model2.predict(y)
+    print(df2[model2.labels_ == pred].head(10))
+else:
+    print(df[model.labels_ == pred].head())
+
 print(pred)
 
-pickle.dump(model,open("assets/recommender_model_kmeans.pkl","wb"))
+title = "Jordans"
+y = vectorizer.transform([title])
+pred = model.predict(y)
+if(pred == largestClusterId):
+    y = vectorizerCluster.transform([title])
+    pred = model2.predict(y)
+    print(df2[model2.labels_ == pred].head(10))
+else:
+    print(df[pred].head())
+    print(df[model.labels_ == pred].head())
+
+print(pred)
+
+if(os.path.exists("assets/recommender_model_kmeans.pkl") == False):
+    pickle.dump(model, open("assets/recommender_model_kmeans.pkl", "wb"))
+pickle.dump(model2, open("assets/resolveBigCluster.pkl", "wb"))
